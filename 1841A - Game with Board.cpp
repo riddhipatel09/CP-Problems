8fc58fb297444b6e7f4f68809b9733b157a216e6
riddhipatel09 <116479348+riddhipatel09@users.noreply.github.com>
2023-09-16 18:59:38 +0530

    Create 	1841A - Game with Board.cpp

---
+++ 1841A - Game with Board.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+    int n;
+    cin >> n;
+        if(n > 5){
+            cout << "Alice" << endl;
+        }
+        else{
+            cout << "Bob" << endl;
+        }
+    }
+    return 0;
+}
